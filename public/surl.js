75017f482644d3155a8e74be393bc017b5e3ef06
Tim Ings <tim@tim-ings.com>
2020-08-25 17:12:05 +0800

    Adds throbber

--- public/surl.js
+++ public/surl.js
@@ -1,7 +1,27 @@
-let linkState = "";
-let errorState = "";
 const linkTag = document.getElementById("short-link");
 const errorTag = document.getElementById("error");
+const loaderTag = document.getElementById("loader");
+const urlInpTag = document.getElementById("inp-url");
+const slugInpTag = document.getElementById("inp-slug");
+
+const initialState = {
+    link: "",
+    error: "",
+    loading: false,
+}
+
+let state = {
+    ...initialState,
+}
+
+const setState = (newState) => {
+    state = newState;
+
+    linkTag.classList.toggle("hidden", !(state.link && state.link.length > 0));
+    loaderTag.classList.toggle("hidden", !state.loading);
+    linkTag.value = state.link;
+    errorTag.textContent = state.error;
+}
 
 window.onload = () => {
     if (window.location.search.indexOf("iframe=true") !== -1) {
@@ -11,37 +31,46 @@ window.onload = () => {
     }
 }
 
-const updateRender = () => {
-    linkTag.classList.toggle("hidden", !(linkState && linkState.length > 0));
-    linkTag.value = linkState;
-    errorTag.textContent = errorState;
-}
-
-const setLink = (url) => {
-    linkState = url;
-    errorState = "";
-    updateRender();
-}
+const setLink = (url) =>
+    setState({
+        link: url,
+        error: "",
+        loading: false,
+    });
 
 const copyLink = () => {
-    updateRender();
+    setState(state);
     linkTag.select();
     linkTag.setSelectionRange(0, 99999);
     document.execCommand("copy");
 }
 
-const setError = (err) => {
-    errorState = err;
-    linkState = "";
-    console.error(errorState);
-    updateRender();
-}
+const setError = (err) =>
+    setState({
+        ...state,
+        link: "",
+        loading: false,
+        error: err,
+    });
+
+const readForm = () => [urlInpTag.value, slugInpTag.value];
+
+const resetApp = () => setState({ ...initialState });
 
 document.getElementById("create-form").addEventListener("submit", async (e) => {
     e.preventDefault();
     setLink("");
-    const url = document.getElementById("inp-url").value;
-    const slug = document.getElementById("inp-slug").value;
+
+    const [url, slug] = readForm();
+    if (!url || url.length <= 0) {
+        return setError("Please enter a URL");
+    }
+
+    setState({
+        ...state,
+        loading: true,
+    });
+    
     try {
         const resp = await fetch("https://surl.tim-ings.com/run", {
             method: "POST",
@@ -55,9 +84,13 @@ document.getElementById("create-form").addEventListener("submit", async (e) => {
             }),
         });
         const data = await resp.json();
-        console.log(data);
         resp.ok ? setLink(data.url) : setError(data.message);
     } catch (err) {
         setError(err.message);
+    } finally {
+        setState({
+            ...state,
+            loading: false,
+        });
     }
 });
